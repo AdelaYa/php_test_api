a9abf8314e9159a921f5e9b8d86d524cd72a0b09
adela-ya <adelya.yarullina@metreshka.ru>
2023-02-21 12:39:13 +0300

    edit project

--- test/mvc/Views/template_view.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <link rel='stylesheet' href='test/style.css'/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
-<?php include 'login_view.php' ; ?>
